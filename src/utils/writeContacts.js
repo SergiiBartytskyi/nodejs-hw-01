3bfd7813d89f56b84e3fcaa3313ee38695cec785
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2024-11-23 01:30:42 +0100

    fix

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,7 +1,5 @@
 import { writeFile } from 'node:fs/promises';
 import { PATH_DB } from '../contacts/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {
-  const results = await writeFile(PATH_DB, JSON.stringify(updatedContacts));
-  return results;
-};
+export const writeContacts = (updatedContacts) =>
+  writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
